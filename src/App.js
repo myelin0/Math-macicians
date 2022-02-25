92fc2d96cd429ff31d7ea0ad3bbb46785bdc7288
myelin0 <emi.veisllari@gmail.com>
2022-02-26 00:50:59 +0100

    Add events to calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React from 'react';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Calculator />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+
+    );
+  }
 }
 
 export default App;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,36 +1,63 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 import './calculator.css';
 
-function Calculator() {
-  return (
-    <div className="container">
-      <div className="calc-grid">
-        <div className="output">
-          <div className="current-operand">0</div>
-        </div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="orange">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="orange">*</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="orange">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="orange">+</button>
-        <button type="button" className="span-two">0</button>
-        <button type="button">.</button>
-        <button type="button" className="orange">=</button>
-      </div>
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
 
-    </div>
-  );
+ clickHandler = (e) => {
+   const data = calculate(this.state, e.target.innerText);
+   this.setState(data);
+ };
+
+ render() {
+   const { total, operation, next } = this.state;
+   return (
+     <div className="container">
+       <div className="calc-grid">
+         <div className="output">
+           {`${total || ''} ${operation || ''} ${next || ''}`}
+           {/* <div className="current-operand">0</div> */}
+         </div>
+         <div
+           className="input"
+           role="button"
+           tabIndex={0}
+           onClick={this.clickHandler}
+           onKeyDown={this.clickHandler}
+         >
+           <button type="button">AC</button>
+           <button type="button">+/-</button>
+           <button type="button">%</button>
+           <button type="button" className="orange">÷</button>
+           <button type="button">7</button>
+           <button type="button">8</button>
+           <button type="button">9</button>
+           <button type="button" className="orange">*</button>
+           <button type="button">4</button>
+           <button type="button">5</button>
+           <button type="button">6</button>
+           <button type="button" className="orange">-</button>
+           <button type="button">1</button>
+           <button type="button">2</button>
+           <button type="button">3</button>
+           <button type="button" className="orange">+</button>
+           <button type="button" className="span-two">0</button>
+           <button type="button">.</button>
+           <button type="button" className="orange">=</button>
+         </div>
+       </div>
+     </div>
+   );
+ }
 }
 
 export default Calculator;
