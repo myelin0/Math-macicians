3b5e8011b5e4330afa2a2e5b77d6bad6b6dc3bc6
myelin0 <emi.veisllari@gmail.com>
2022-03-01 15:02:46 +0100

    Add router to Home and Quotes files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import Calculator from './components/calculator';
+import { Routes, Route, Link } from 'react-router-dom';
+// import Calculator from './components/calculator';
+import Home from './Home';
+import Quote from './Quote';
 
-const App = () => <Calculator />;
-
+const App = () => (
+  <>
+    <div>MATH MAGICIANS</div>
+    <nav>
+      <Link to="/">Home |</Link>
+      <Link to="/Quotes">Calculator |</Link>
+      <Link to="/Quotes">Quotes |</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/Calculator" element={<Calculator />} /> */}
+      <Route path="/Quotes" element={<Quote />} />
+    </Routes>
+  </>
+);
 export default App;
---
+++ src/Quote.js
@@ -0,0 +1,5 @@
+const will = 'Mathematics is not about numbers,equations,computations,or algorithms;it is about understanding - William Paul Thurston';
+const Quote = () => (
+  <p>{will}</p>
+);
+export default Quote;
