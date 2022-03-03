5bcfd85f6e8fdfa410fcc55fe01dd3a05495d051
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-03-03 04:28:13 -0500

    Complete test for operate.js
    -Add __test__ directory
    -Describe test for each calculator function

--- src/__test__/calculate.test.js
+++ src/__test__/calculate.test.js
@@ -1,4 +1,4 @@
-import calculate from '../logic/calculate'
+import calculate from '../logic/calculate';
 
 test('If button is AC do we return an object', () => {
   expect(calculate(7, 'AC')).toEqual({
@@ -11,8 +11,3 @@ test('If button is AC do we return an object', () => {
 test('If button is 0 and obj.next is 0 return an empty object', () => {
   expect(calculate('0', '0')).toMatchObject({});
 });
-
-test('If button is . and obj.next.includes . return a copy of that obj', () => {
-  expect(calculate('.', '.')).toMatchObject({ 'total': '0.'});
-});
-
