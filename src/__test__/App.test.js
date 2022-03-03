2cfa680d2327f3cb41e965c58f8e9887b9e8e780
myelin0 <emi.veisllari@gmail.com>
2022-03-03 17:56:10 +0100

    Test App with react library

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import {MemoryRouter} from 'react-router-dom';
-// import { createMemoryHistory } from 'history';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
-
 import '@testing-library/jest-dom';
 
 describe('App', () => {
@@ -18,4 +16,21 @@ describe('App', () => {
     expect(screen.queryByText('William Paul Thurston')).toBeNull();
     expect(screen.queryByText("Let's do some math!")).toBeNull();
   });
+});
+
+test('test hyperlink click', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  fireEvent.click(screen.getByText('Calculator'));
+
+  expect(screen.getByText('Calculator')).toMatchInlineSnapshot(`
+<a
+  href="/Calculator"
+>
+  Calculator 
+</a>
+`);
 });
