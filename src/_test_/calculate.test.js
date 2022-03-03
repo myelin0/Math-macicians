16bf5844cddf040cd90a5860f916a2bd29b6042c
myelin0 <emi.veisllari@gmail.com>
2022-03-03 09:28:49 +0100

    Unit testing calculate.js functions

--- src/_test_/calculate.test.js
+++ src/_test_/calculate.test.js
@@ -1,4 +1,4 @@
-import calculate from '../logic/calculate'
+import calculate from '../logic/calculate';
 
 test('If button is AC do we return an object', () => {
   expect(calculate(7, 'AC')).toEqual({
@@ -12,7 +12,76 @@ test('If button is 0 and obj.next is 0 return an empty object', () => {
   expect(calculate('0', '0')).toMatchObject({});
 });
 
-test('If button is . and obj.next.includes . return a copy of that obj', () => {
-  expect(calculate('.', '.')).toMatchObject({ 'total': '0.'});
-});
+let inputObj = {
+  total: null,
+  next: null,
+  operation: null,
+};
 
+describe('Calculator functions test', () => {
+  test('Test the sum ', () => {
+    inputObj = {
+      total: '3',
+      next: '2',
+      operation: '+',
+    };
+
+    const { total } = calculate(inputObj, '=');
+    expect(total).toBe('5');
+  });
+
+  test('Test subtraction ', () => {
+    inputObj = {
+      total: '3',
+      next: '2',
+      operation: '-',
+    };
+
+    const { total } = calculate(inputObj, '=');
+    expect(total).toBe('1');
+  });
+
+  test('Test division ', () => {
+    inputObj = {
+      total: '4',
+      next: '2',
+      operation: '÷',
+    };
+
+    const { total } = calculate(inputObj, '=');
+    expect(total).toBe('2');
+  });
+
+  test('Tests multiplication operation', () => {
+    inputObj = {
+      total: '2',
+      next: '1',
+      operation: 'x',
+    };
+
+    const { total } = calculate(inputObj, '=');
+    expect(total).toBe('2');
+  });
+
+  test('Tests modulus operation', () => {
+    inputObj = {
+      total: '5',
+      next: '2',
+      operation: '%',
+    };
+
+    const { total } = calculate(inputObj, '=');
+    expect(total).toBe('1');
+  });
+
+  test('Tests +- button', () => {
+    inputObj = {
+      total: '1',
+      next: '5',
+      operation: '-',
+    };
+
+    const { next } = calculate(inputObj, '+/-');
+    expect(next).toBe('-5');
+  });
+});
